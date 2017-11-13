dee10b57bf540ddb0bf9591c1a51653513469280
Long Mai <longility@gmail.com>
2017-11-12 22:07:04 -0600

    clean up send demo

--- MicroBus.SenderDemo/Program.cs
+++ MicroBus.SenderDemo/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using MicroBus.Abstractions;
 using MicroBus.DemoMessages;
@@ -9,60 +9,112 @@ namespace MicroBus.SenderDemo
 {
     class Program
     {
-        class BlahMessageHandler : IMessageHandler<AppleCommand>
-        {
-            public Task Handle(AppleCommand message)
-            {
-                throw new NotImplementedException();
-            }
-        }
-        static async Task Main(string[] args)
-        {
-            var subscriptionId = "";
-            var tenantId = "";
-            var clientId = "";
-            var clientSecret = "";
+        private const string SubscriptionId = "";
+        private const string TenantId = "";
+        private const string ClientId = "";
+        private const string ClientSecret = "";
+        private const string ConnectionString = "";
 
-            var serviceCollection = new ServiceCollection();
-
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-            serviceCollection.AddSingleton(p =>
-                {
-                    return new BusBuilder()
+        static async Task<IBus> JupiterService()
+        {
+            var bus = new BusBuilder()
                         .UseAzureServiceBus(
                             "",
                             configuration =>
                             {
-                                configuration.SubscriptionId = subscriptionId;
-                                configuration.TenantId = tenantId;
-                                configuration.ClientId = clientId;
-                                configuration.ClientSecret = clientSecret;
+                                configuration.SubscriptionId = SubscriptionId;
+                                configuration.TenantId = TenantId;
+                                configuration.ClientId = ClientId;
+                                configuration.ClientSecret = ClientSecret;
 
                                 configuration.PublishAndSendOptions
-                                             .MapCommandToQueue<AppleCommand>("receiver")
-                                           .MapCommandToQueue<BananaCommand>("randomreceiver");
+                                             .MapCommandToQueue<AppleCommand>("saturn");
                             })
-                        .UseMicrosoftDependencyInjection(p)
-                        .AddMessageHandler(() => new BlahMessageHandler()) //IMessageHandler<Message>,new() (cannot have duplicate handlers on a message)
-
                         .Build();
-                });
+            await bus.Start();
+
+            await bus.Publish(new CatEvent { Text = $"Meow published at {DateTimeOffset.UtcNow}" });
+            await bus.Send(new AppleCommand { Text = $"jupiter to saturn sent at {DateTimeOffset.UtcNow}" });
+
+            return bus;
+        }
+
+        class AppleCommandHandler : IMessageHandler<AppleCommand>
+        {
+            public Task Handle(AppleCommand message)
+            {
+                Trace.WriteLine($"AppleCommand received with message '{message.Text}' at {DateTimeOffset.UtcNow}");
+
+                return Task.CompletedTask;
+            }
+        }
+
+        class RottenAppleCommandHandler : IMessageHandler<AppleCommand>
+        {
+            public Task Handle(AppleCommand message)
+            {
+
+                Trace.WriteLine($"AppleCommand received with message '{message.Text}' at {DateTimeOffset.UtcNow}");
+                throw new Exception("Rotten Apple");
+            }
+        }
+
+        class CatEventHandler : IMessageHandler<CatEvent>
+        {
+            public Task Handle(CatEvent message)
+            {
+                Trace.WriteLine($"CatEvent received with message '{message.Text}' at {DateTimeOffset.UtcNow}");
+
+                return Task.CompletedTask;
+            }
+        }
+
+        static async Task<IBus> SaturnService()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddScoped<AppleCommandHandler>()
+                .AddScoped<CatEventHandler>()
+                .AddSingleton(p =>
+                    {
+                        return new BusBuilder()
+                            .UseAzureServiceBus(
+                                "",
+                                configuration =>
+                                {
+                                    configuration.SubscriptionId = SubscriptionId;
+                                    configuration.TenantId = TenantId;
+                                    configuration.ClientId = ClientId;
+                                    configuration.ClientSecret = ClientSecret;
+
+                                    configuration.ReceiveOptions.QueueName = "saturn";
+                                })
+                            .UseMicrosoftDependencyInjection(p)
+                            .Build();
+                    });
+            var serviceProvider = serviceCollection.BuildServiceProvider();
             var bus = serviceProvider.GetService<IBus>();
-            //TECHNICAL ISSUE: May not be able to do same pattern with other dependency injection frameworks.
 
             await bus.Start();
-            await bus.Publish(new CatEvent { Text = "This is event." });
-            await bus.Publish(new DogEvent { Text = "This is event 2." });
-            await bus.Send(new AppleCommand { Text = "This is command." });
-            await bus.Send(new BananaCommand { Text = "This is command 2." });
 
-            //samples
-            //Retry policy
-            //Test different .net frameworks as consumer
-            //GitHub
-            //Documentation
-            //Deploy to nuget server
-            //Error handling /DLQ
+            return bus;
+        }
+
+        static async Task Main(string[] args)
+        {
+            var buses = new[]
+            {
+                await JupiterService(),
+                await SaturnService()
+            };
+
+            Console.WriteLine("Press any key to end.");
+            Console.Read();
+
+            foreach (var bus in buses)
+            {
+                await bus.Stop();
+            }
         }
     }
 }
