3e07b60cab2ac91d24e19ebb79e180d7424e5aaa
Long Mai <longility@gmail.com>
2017-11-22 09:42:47 -0600

    add message to exception handling

--- RockyBus/MessageHandler/MessageHandlingExceptionRaisedEventArgs.cs
+++ RockyBus/MessageHandler/MessageHandlingExceptionRaisedEventArgs.cs
@@ -4,6 +4,7 @@ namespace RockyBus
     public class MessageHandlingExceptionRaisedEventArgs
     {
         public Type MessageHandlerType { get; internal set; }
+        public object Message { get; internal set; }
         public Exception Exception { get; internal set; }
     }
 }
